a3a536f7678c1fc33e73f5f1f6d9484ec7e32b6b
chudst <chudst@gmail.com>
2026-02-03 17:36:41 +0100

    Update edeska_jmk.py

--- edeska_jmk.py
+++ edeska_jmk.py
@@ -33,8 +33,8 @@ MODE = 'range'
 
 # Pro rucni rezim (MODE = 'range'):
 # Format: YYYY-MM-DD
-RANGE_FROM = '2025-01-30'
-RANGE_TO = '2025-01-30'
+RANGE_FROM = '2026-01-30'
+RANGE_TO = '2065-01-30'
 
 # ================== VYPOCET DAT ==================
 
